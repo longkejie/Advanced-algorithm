0d6a55d4b32382c54633e3a142fa476ff774612f
longkejie <1721248012@qq.com>
2021-01-13 14:37:46 +0800

    字符串匹配算法code

--- 字符串匹配算法/code/string_match.cpp
+++ 字符串匹配算法/code/string_match.cpp
@@ -29,6 +29,25 @@ int brute_force(const char *s, const char *t) {
     return -1;
 }
 
+int getNext(const char *t, int &j, char input, int *next) {
+    while (j != -1 && t[j + 1] != input) j = next[j];
+    if (t[j + 1] == input) j = j + 1;
+    return j;
+}
+
+int kmp(const char *s, const char *t) {
+    int n = strlen(t);
+    int *next = (int *)malloc(sizeof(int) * (n + 1));
+    memset(next, -1, sizeof(next));
+    next[0] = -1;
+    for (int i = 1, j = -1; t[i]; ++i) next[i] = getNext(t, j, t[i], next);
+    for (int i = 0, j = -1; s[i]; ++i) {
+        if (getNext(t, j, s[i], next) != n - 1) continue;
+        return i - n + 1;
+    }    
+    return -1;
+}
+
 int sunday(const char *s, const char *t) {
     int n = strlen(t);
     int m = strlen(s);
@@ -51,6 +70,7 @@ int main () {
 
     while (scanf("%s %s", s, t)) {
         printf("brute_force(%s %s) = %d\n", s, t, brute_force(s,t) );
+        printf("KMP(%s %s) = %d\n", s, t, kmp(s,t));
         printf("sunday(%s %s) = %d\n", s, t, sunday(s,t));
     }
 
