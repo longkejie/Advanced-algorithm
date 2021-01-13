e1f82681f097a1951bfa5d2f38690ea934923156
longkejie <1721248012@qq.com>
2021-01-13 12:29:41 +0800

    字符串匹配算法

---
+++ 字符串匹配算法/code/string_match.cpp
@@ -0,0 +1,58 @@
+/*************************************************************************
+	> File Name: string_match.cpp
+	> Author: longkejie
+	> Mail:1721248012@qq.com 
+	> Created Time: Wed 13 Jan 2021 12:11:29 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<cstring>
+#include<map>
+#include<queue>
+#include<stack>
+#include<algorithm>
+#include<set>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+int brute_force(const char *s, const char *t) {
+    for (int i = 0; s[i]; ++i) {
+        int flag = 1;
+        for (int j = 0; t[j]; ++j) {
+            if (t[j] == s[i + j]) continue;
+            flag = 0;
+            break;
+        }
+        if (flag) return i;
+    }
+    return -1;
+}
+
+int sunday(const char *s, const char *t) {
+    int n = strlen(t);
+    int m = strlen(s);
+    int offset[256];
+    for (int i = 0; i < 256; ++i) offset[i] = n + 1;
+    for (int i = 0; t[i]; ++i) offset[t[i]] = n - i;
+    for (int i = 0; i + n <= m; i += offset[s[i + n]]) {
+        int flag = 1;
+        for (int j = 0; t[j]; ++j) {
+            flag = flag && (t[j] == s[i + j]);
+        } 
+        if (flag) return i;
+    }
+    return -1;
+}
+
+
+int main () {
+    char s[1000], t[1000];
+
+    while (scanf("%s %s", s, t)) {
+        printf("brute_force(%s %s) = %d\n", s, t, brute_force(s,t) );
+        printf("sunday(%s %s) = %d\n", s, t, sunday(s,t));
+    }
+
+    return 0;
+}
---
+++ 线段树/oj224.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: oj224.cpp
+	> Author: longkejie
+	> Mail:1721248012@qq.com 
+	> Created Time: Wed 30 Dec 2020 03:10:37 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<cstring>
+#include<map>
+#include<queue>
+#include<stack>
+#include<algorithm>
+#include<set>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+
+
+
+int main () {
+    
+    return 0;
+}
